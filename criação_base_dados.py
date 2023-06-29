43fa138373c6dde86ec824af343b8927181f375b
Cwitzz <121115178+Cwitzz@users.noreply.github.com>
2023-06-29 14:27:49 -0300

    Vs2

--- criação_base_dados.py
+++ criação_base_dados.py
@@ -89,7 +89,133 @@ livros = [
         'Ano Publicação': 1950,
         'Gênero': 'Fantasia',
         'Preço': 20.99
-    }
+    },
+[    {
+        'Título': 'A Metamorfose',
+        'Autor': 'Franz Kafka',
+        'Ano Publicação': 1915,
+        'Gênero': 'Ficção',
+        'Preço': 13.99    },
+     {
+         'Título': 'A Guerra dos Tronos',
+         'Autor': 'George R.R. Martin',
+         'Ano Publicação': 1996,
+         'Gênero': 'Fantasia',
+         'Preço': 25.99    },
+    {
+         'Título': 'O Retrato de Dorian Gray',
+         'Autor': 'Oscar Wilde',
+         'Ano Publicação': 1890,
+         'Gênero': 'Clássico',
+         'Preço': 16.99    },
+    {
+         'Título': '1984',
+         'Autor': 'George Orwell',
+         'Ano Publicação': 1949,
+         'Gênero': 'Ficção',
+         'Preço': 19.99    },
+    {
+         'Título': 'Harry Potter e a Câmara Secreta',
+         'Autor': 'J.K. Rowling',
+         'Ano Publicação': 1998,
+         'Gênero': 'Fantasia',
+         'Preço': 31.99    },
+    {
+         'Título': 'O Hobbit',
+         'Autor': 'J.R.R. Tolkien',
+         'Ano Publicação': 1937,
+         'Gênero': 'Fantasia',
+         'Preço': 26.99    },
+    {
+         'Título': 'Romeu e Julieta',
+         'Autor': 'William Shakespeare',
+         'Ano Publicação': 1597,
+         'Gênero': 'Romance',
+         'Preço': 15.99    },
+    {
+         'Título': 'Os Miseráveis',
+         'Autor': 'Victor Hugo',
+         'Ano Publicação': 1862,
+         'Gênero': 'Clássico',
+         'Preço': 27.99    },
+    {
+         'Título': 'O Senhor dos Anéis: As Duas Torres',
+         'Autor': 'J.R.R. Tolkien',
+         'Ano Publicação': 1954,
+         'Gênero': 'Fantasia',
+         'Preço': 24.99    },
+    {
+         'Título': 'A Revolução dos Bichos',
+         'Autor': 'George Orwell',
+         'Ano Publicação': 1945,
+         'Gênero': 'Ficção',
+         'Preço': 17.99    },
+    {
+        'Título': 'A Ilíada',
+        'Autor': 'Homero',
+        'Ano Publicação': -762,
+        'Gênero': 'Clássico',
+        'Preço': 11.99    },
+    {
+        'Título': 'O Nome do Vento',
+        'Autor': 'Patrick Rothfuss',
+        'Ano Publicação': 2007,
+        'Gênero': 'Fantasia',
+        'Preço': 23.99    },
+    {
+        'Título': 'Dom Quixote',
+        'Autor': 'Miguel de Cervantes',
+        'Ano Publicação': 1605,
+        'Gênero': 'Clássico',
+        'Preço': 12.99    },
+    {
+        'Título': 'O Pequeno Príncipe',
+        'Autor': 'Antoine de Saint-Exupéry',
+        'Ano Publicação': 1943,
+        'Gênero': 'Infantil',
+        'Preço': 9.99    },
+    {
+        'Título': 'Hamlet',
+        'Autor': 'William Shakespeare',
+        'Ano Publicação': 1603,
+        'Gênero': 'Tragédia',
+        'Preço': 14.99    },
+    {
+        'Título': 'Cem Anos de Solidão',
+        'Autor': 'Gabriel García Márquez',
+        'Ano Publicação': 1967,
+        'Gênero': 'Realismo',
+        'Preço': 21.99    },
+    {
+        'Título': 'Percy Jackson e o Ladrão de Raios',
+        'Autor': 'Rick Riordan',
+        'Ano Publicação': 2005,
+        'Gênero': 'Fantasia',
+        'Preço': 16.99    },
+    {
+        'Título': 'A Culpa é das Estrelas',
+        'Autor': 'John Green',
+        'Ano Publicação': 2012,
+        'Gênero': 'Romance',
+        'Preço': 18.99    },
+    {
+        'Título': 'O Alquimista',
+        'Autor': 'Paulo Coelho',
+        'Ano Publicação': 1988,
+        'Gênero': 'Autoajuda',
+        'Preço': 15.99    },
+    {
+        'Título': 'O Código Da Vinci',
+        'Autor': 'Dan Brown',
+        'Ano Publicação': 2003,
+        'Gênero': 'Suspense',
+        'Preço': 22.99    },
+    {
+        'Título': 'As Crônicas de Nárnia',
+        'Autor': 'C.S. Lewis',
+        'Ano Publicação': 1950,
+        'Gênero': 'Fantasia',
+        'Preço': 20.99    }]
 ]
 
 # Nome do arquivo
