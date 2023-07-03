c1ac5a8ab70f65e845329c4a5841af9922ac4718
Cwitzz <121115178+Cwitzz@users.noreply.github.com>
2023-07-03 18:43:12 -0300

    Quebrando o arquivo data_cleaning_transform em microfunções para facilitar os testes

--- data_cleaning_transform.py
+++ data_cleaning_transform.py
@@ -4,43 +4,62 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 
-# Conectar ao banco de dados SQLite
-conn = sqlite3.connect('DBFIC.db')
 
-# Etapa 1: Extração de Dados
-data = pd.read_sql_query("SELECT * FROM transacoes", conn)
+def connect_to_db(db_name):
+    return sqlite3.connect(db_name)
 
-# Etapa 2: Identificar Valores Nulos
-missing_data = data.isnull().sum()
 
-# Etapa 3: Preparação de Dados para o K-means (Normalização)
-numerical_features = ['quantity', 'price']  # lista das colunas numéricas
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(data[numerical_features])
+def extract_data(conn):
+    return pd.read_sql_query("SELECT * FROM transacoes", conn)
 
-# Etapa 4: Aplicar K-means para Imputação
-kmeans = KMeans(n_clusters=3, n_init=10, random_state=0).fit(scaled_data)
-data['cluster'] = kmeans.labels_
 
-# Preenchendo os valores nulos com a média dos valores do cluster correspondente
-imputer = SimpleImputer(strategy='mean')
-data[numerical_features] = imputer.fit_transform(data.groupby('cluster')[numerical_features].transform(lambda x: x.fillna(x.mean())))
+def scale_numerical_data(data, numerical_features):
+    scaler = StandardScaler()
+    return scaler.fit_transform(data[numerical_features])
 
-# Etapa 5: Correção de formatos de dados
-# Converter 'quantity' para int
-data['quantity'] = data['quantity'].astype(int)
 
-# Converter 'price' para float
-data['price'] = data['price'].astype(float)
+def apply_kmeans(scaled_data, n_clusters=3):
+    kmeans = KMeans(n_clusters=n_clusters, n_init=10, random_state=0).fit(scaled_data)
+    return kmeans.labels_
 
-# Converter 'date' para o formato padrão de data YYYY-MM-DD
-data['date'] = pd.to_datetime(data['date']).dt.strftime('%Y-%m-%d')
 
-# Etapa 6: Transformar a coluna 'sexo' em valores binários
-data['sex'] = data['sex'].map({'male': 0, 'female': 1})
+def impute_missing_values(data, numerical_features):
+    imputer = SimpleImputer(strategy='mean')
+    return imputer.fit_transform(data.groupby('cluster')[numerical_features].transform(lambda x: x.fillna(x.mean())))
 
-# Etapa 7: Carregar os Dados Transformados de Volta ao SQLite
-data.to_sql('transacoes_imputadas', conn, if_exists='replace', index=False)
 
-# Fechar a conexão com o banco de dados
-conn.close()
+def convert_data_formats(data):
+    data['quantity'] = data['quantity'].astype(int)
+    data['price'] = data['price'].astype(float)
+    data['date'] = pd.to_datetime(data['date']).dt.strftime('%Y-%m-%d')
+    return data
+
+
+def transform_sex_column(data):
+    data['sex'] = data['sex'].map({'male': 0, 'female': 1})
+    return data
+
+
+def load_data_to_sqlite(data, conn):
+    data.to_sql('transacoes_imputadas', conn, if_exists='replace', index=False)
+
+
+def main():
+    conn = connect_to_db('DBFIC.db')
+    data = extract_data(conn)
+    numerical_features = ['quantity', 'price']
+
+    scaled_data = scale_numerical_data(data, numerical_features)
+    data['cluster'] = apply_kmeans(scaled_data)
+
+    data[numerical_features] = impute_missing_values(data, numerical_features)
+    data = convert_data_formats(data)
+    data = transform_sex_column(data)
+
+    load_data_to_sqlite(data, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
