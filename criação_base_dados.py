8c2670fdf4a64b46a3446d42c5f401db82d2daf2
Cwitzz <121115178+Cwitzz@users.noreply.github.com>
2023-07-04 10:44:40 -0300

    .

--- criação_base_dados.py
+++ criação_base_dados.py
@@ -27,29 +27,25 @@ produtos_jardim = [
     "Rede de proteção para frutas"
 ]
 
-data = [{
-    "transaction_id": i,
-    "customer_name": fake.name(),
-    "customer_address": fake.address().replace('\n', ', '),
-    "product": random.choice(produtos_jardim),
-    "quantity": int(fake.random_digit_not_null()),
-    "price": float(fake.random_number(digits=4)) / 100,
-    "date": str(fake.date_between(start_date='-1y', end_date='today')),
-    "sex": random.choice(['Masculino', 'Feminino'])
-} for i in range(1000)]
-
-
 # Conectar ao banco de dados SQLite
-# Nota: substitua 'DBFIC.db' pelo nome do seu arquivo de banco de dados
 conn = sqlite3.connect('DBFIC.db')
 cursor = conn.cursor()
 
 # Inserir os dados no banco de dados
-for item in data:
+for i in range(1000):
+    transaction_id = i
+    customer_name = fake.name()
+    customer_address = fake.address().replace('\n', ', ')
+    product = random.choice(produtos_jardim)
+    quantity = int(fake.random_digit_not_null())
+    price = float(fake.random_number(digits=4)) / 100
+    date = str(fake.date_between(start_date='-1y', end_date='today'))
+    sex = random.choice(['Masculino', 'Feminino'])
+
     cursor.execute('''
-        INSERT INTO transacoes (transaction_id, customer_name, customer_address, product, quantity, price, date, sexo)
+        INSERT INTO transacoes (transaction_id, customer_name, customer_address, product, quantity, price, date, sex)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (item['transaction_id'], item['customer_name'], item['customer_address'], item['product'], item['quantity'], item['price'], item['date'], item['sex']))
+    ''', (transaction_id, customer_name, customer_address, product, quantity, price, date, sex))
 
 # Fazer commit das alterações
 conn.commit()
