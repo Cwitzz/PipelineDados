2ec86af4b4893343c1f9dc3cd01f6217515ef2be
Cwitzz <121115178+Cwitzz@users.noreply.github.com>
2023-07-04 09:05:31 -0300

    Removendo encoders 'customer_name', 'product'

--- model_training_product_recomm.py
+++ model_training_product_recomm.py
@@ -4,7 +4,6 @@ import joblib
 import logging
 import time
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
@@ -17,15 +16,6 @@ def connect_to_db(db_name):
 
 def load_data(conn, query):
     return pd.read_sql_query(query, conn)
-
-
-def encode_features(data, columns):
-    encoders = {}
-    for col in columns:
-        encoder = LabelEncoder()
-        data[col] = encoder.fit_transform(data[col])
-        encoders[col] = encoder
-    return data, encoders
 
 
 def train_model(X_train, y_train, n_estimators=100):
@@ -41,27 +31,19 @@ def validate_model(model, X_test, y_test):
     return mse
 
 
-def generate_recommendations(model, data, customer_encoder, product_encoder, top_n=5):
+def generate_recommendations(model, data, top_n=5):
     recommendations_all = []
-    unique_customers = customer_encoder.classes_
-    unique_products = product_encoder.classes_
+    unique_customers = data['customer_name'].unique()
+    unique_products = data['product'].unique()
 
     for customer_name in unique_customers:
-        encoded_customer_name = customer_encoder.transform([customer_name])[0]
         recommendations = []
-
-        # Recuperar os valores de cluster e sex para este cliente específico
         customer_data = data[data['customer_name'] == customer_name].iloc[0]
         cluster = customer_data['cluster']
         sex = customer_data['sex']
 
         for product in unique_products:
-            encoded_product = product_encoder.transform([product])[0]
-            # Desfazer a transformação dos encoders
-            original_customer_name = customer_encoder.inverse_transform([encoded_customer_name])[0]
-            original_product = product_encoder.inverse_transform([encoded_product])[0]
-            # Incluindo 'cluster' e 'sex' como recursos para previsão
-            features = [original_customer_name, original_product, cluster, sex]
+            features = [customer_name, product, cluster, sex]
             predicted_rating = model.predict([features])[0]
             recommendations.append((product, predicted_rating))
 
@@ -78,7 +60,6 @@ def main():
     conn = connect_to_db('DBFIC.db')
     data = load_data(conn, "SELECT * FROM transacoes_imputadas")
 
-    data, encoders = encode_features(data, ['customer_name', 'product', 'cluster', 'sex'])
     X = data[['customer_name', 'product', 'cluster', 'sex']]
     y = data['price']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -86,7 +67,7 @@ def main():
     model = train_model(X_train, y_train, n_estimators=100)
     validate_model(model, X_test, y_test)
 
-    recommendations = generate_recommendations(model, data, encoders['customer_name'], encoders['product'])
+    recommendations = generate_recommendations(model, data)
 
     for customer_name, recs in recommendations:
         logging.info(f"Recomendações para o cliente {customer_name}:")
@@ -94,8 +75,6 @@ def main():
             logging.info(f"{i}. Produto: {item} | Rating previsto: {rating}")
 
     joblib.dump(model, 'trained_model.pkl')
-    joblib.dump(encoders['customer_name'], 'customer_encoder.pkl')
-    joblib.dump(encoders['product'], 'product_encoder.pkl')
 
     conn.close()
 
